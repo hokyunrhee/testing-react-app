307806b2da5889eca32742ea7016f6809ccd6dea
hokyunrhee <hokyun.rhee@gmail.com>
2021-07-24 20:40:59 +0900

    test: correct minor typo in folder name

--- components/todo-list/todo-list.spec.jsx
+++ components/todo-list/todo-list.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import { TodoList } from './toto-list';
+import { TodoList } from './todo-list';
 
 describe('TodoList Component', () => {
   const todos = [
