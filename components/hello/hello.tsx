551630dcd6fd32bec3239bff66e43df139e1597c
hokyunrhee <hokyun.rhee@gmail.com>
2021-07-27 09:21:57 +0900

    chore: delete hello component

--- components/hello/hello.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export interface HelloProps {
-  userName: string;
-}
-
-export const Hello = ({ userName }: HelloProps) => {
-  return <div data-cy="greeting">Hello, {userName}!</div>;
-};
